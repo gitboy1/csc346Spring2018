f07a104105f0d2b936fb168c272c8c2b42a112a7
gitboy1 <jcarlisle2@missouriwestern.edu>
2018-02-11 14:32:49 -0600

    Jon's first successful commit

---
+++ IdeaProjects/CSC346 MidTerm Part A/src/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+//    Git commit 2
+    public static void main(String[] args) {
+        System.out.println("Another Hello world");
+        System.out.println("Another Hello hello");
+        System.out.println("Another Howdy");
+    }
+}
